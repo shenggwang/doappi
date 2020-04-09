24527a16f3ab3ddbf58e4194264da6f9a53c7c38
shenggwang <shenggwangg@gmail.com>
2020-04-09 01:26:44 +0100

    docker compose

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t docker-ap .
+docker build -t doappi .
 
-sudo docker run --name docker-ap -d -ti -e INTERFACE=wlan0 --net host --privileged docker-ap
-#sudo docker run --name docker-ap -ti -e INTERFACE=wlan0 --net host --privileged docker-ap
+#sudo docker run --name doappi -d -ti -e INTERFACE=wlan0 --net host --privileged doappi
+sudo docker run --name doappi -d -ti --net host --privileged doappi
